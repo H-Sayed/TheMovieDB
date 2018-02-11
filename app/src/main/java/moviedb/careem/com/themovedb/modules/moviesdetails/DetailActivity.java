718bf702e0a04aa42a73c57d511610a29eb46a61
HassanSayed <tweets.elmasry@gmail.com>
2018-02-11 22:34:37 +0200

    remove unused imports

--- app/src/main/java/moviedb/careem/com/themovedb/modules/moviesdetails/DetailActivity.java
+++ app/src/main/java/moviedb/careem/com/themovedb/modules/moviesdetails/DetailActivity.java
@@ -3,15 +3,12 @@ package moviedb.careem.com.themovedb.modules.moviesdetails;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
-
 import butterknife.BindView;
 import moviedb.careem.com.themovedb.BuildConfig;
 import moviedb.careem.com.themovedb.R;
@@ -47,7 +44,6 @@ public class DetailActivity extends BaseActivity {
         }
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setMovie();
-
     }
 
     private void setMovie() {
